ebdb77213323a0dc52136191f3b3b79d02dd60fd
connorguy <connor_guy@icloud.com>
2017-11-10 15:57:48 -0800

    Collect user input and output basic string

--- createPass/ViewController.swift
+++ createPass/ViewController.swift
@@ -12,13 +12,13 @@ class ViewController: NSViewController {
 
     @IBOutlet weak var websiteName: NSTextField!
     @IBOutlet weak var userPass: NSSecureTextField!
-    @IBOutlet weak var generate: NSButton!
+    @IBOutlet weak var newPass: NSTextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // Create a new layer for the background fill
         self.view.wantsLayer = true
-
     }
 
     override var representedObject: Any? {
@@ -29,19 +29,29 @@ class ViewController: NSViewController {
     
     override func awakeFromNib() {
         if self.view.layer != nil {
+            // Set background color
             let color : CGColor = CGColor.init(red: 0.4902, green: 0.7451, blue: 0.6471, alpha: 1) /* #7dbea5 */
             self.view.layer?.backgroundColor = color
         }
     }
     
-    func sha256(data : Data) -> Data {
-        var hash = [UInt8](repeating: 0,  count: Int(CC_SHA256_DIGEST_LENGTH))
-        data.withUnsafeBytes {
-            _ = CC_SHA256($0, CC_LONG(data.count), &hash)
-        }
-        return Data(bytes: hash)
+    @IBAction func generate(_ sender: Any) {
+        // Get user input from the text feilds
+        let userWeb = String(websiteName.stringValue)
+        let userPassword = String(userPass.stringValue)
+        
+        // Concatonate the two strings
+        let newPassword = String(userWeb + userPassword)
+        
+        // Call the she256 alg on the string
+        // TODO
+        
+        // Output the new password onto the screen
+        newPass.stringValue = newPassword
+        
+        // Copy the new password to the clipboard
+        // TODO
     }
-
 
 }
 
