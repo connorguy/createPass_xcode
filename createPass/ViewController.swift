5c7e214bed6ccbc7ff16a58942c8a2106181c4e5
connorguy <connor_guy@icloud.com>
2017-11-10 13:53:52 -0800

    Initial commit with basic ui

--- createPass/ViewController.swift
+++ createPass/ViewController.swift
@@ -10,16 +10,36 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var websiteName: NSTextField!
+    @IBOutlet weak var userPass: NSSecureTextField!
+    @IBOutlet weak var generate: NSButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.view.wantsLayer = true
+
     }
 
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
+    }
+    
+    override func awakeFromNib() {
+        if self.view.layer != nil {
+            let color : CGColor = CGColor.init(red: 0.4902, green: 0.7451, blue: 0.6471, alpha: 1) /* #7dbea5 */
+            self.view.layer?.backgroundColor = color
+        }
+    }
+    
+    func sha256(data : Data) -> Data {
+        var hash = [UInt8](repeating: 0,  count: Int(CC_SHA256_DIGEST_LENGTH))
+        data.withUnsafeBytes {
+            _ = CC_SHA256($0, CC_LONG(data.count), &hash)
+        }
+        return Data(bytes: hash)
     }
 
 
