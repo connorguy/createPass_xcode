b9f13f9c228a73bcd1d2bd061cc31de2c3fadf24
connorguy <connor_guy@icloud.com>
2017-11-09 20:28:48 -0800

    Initial Commit

---
+++ createPass/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  createPass
+//
+//  Created by Connor Guy on 11/9/17.
+//  Copyright © 2017 Connor Guy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
